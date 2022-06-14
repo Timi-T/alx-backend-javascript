9b0457d836669dcec2a63a2761bef842bb9d2ce1
Timi-T <timiopebode@gmail.com>
2022-06-14 14:39:52 +0000

    eslint

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,9 +1,9 @@
 export default function createReportObject(employeesList) {
-  let obj = {
-    ["allEmployees"]: employeesList,
-    getNumberOfDepartments () {
-      return (Object.keys(employeesList).length)
+  const obj = {
+    allEmployees: employeesList,
+    getNumberOfDepartments() {
+      return (Object.keys(employeesList).length);
     },
   };
-  return obj
+  return obj;
 }
