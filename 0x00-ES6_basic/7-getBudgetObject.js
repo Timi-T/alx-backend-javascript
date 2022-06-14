700cfc42f2b139591b9e84852f50d03a7f76d89d
Timi-T <timiopebode@gmail.com>
2022-06-14 14:21:47 +0000

    eslint

--- 0x00-ES6_basic/7-getBudgetObject.js
+++ 0x00-ES6_basic/7-getBudgetObject.js
@@ -1,10 +1,8 @@
-//Object property value shorthand syntax
 export default function getBudgetObject(income, gdp, capita) {
   const budget = {
     income,
     gdp,
     capita,
   };
-
   return budget;
 }
