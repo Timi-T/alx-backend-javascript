48313a6cdc907abfa1b79884dbf958f1f5c4d224
Timi-T <timiopebode@gmail.com>
2022-06-15 20:12:01 +0000

    then.catch.finally

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,9 +1,5 @@
 export default function handleResponseFromAPI(promise) {
-  promise.then(
-    (result) => {
-      console.log('Got a response from the API');
-      return ({ status: 200, body: 'Success' });
-    },
-    () => Error(),
-  );
+  return promise.then(() => ({ status: 200, body: 'Success' }))
+    .catch(() => Error())
+    .finally(() => console.log('Got a response from the API'));
 }
