8a49039ef230d270d036c58a803c750ee343d896
Timi-T <timiopebode@gmail.com>
2022-06-15 10:36:49 +0000

    resolve promise

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,7 +1,7 @@
 export default function handleResponseFromAPI(promise) {
   promise.then(() => {
     console.log('Got a response from the API');
-    return { status: 200, body: 'success' };
+    Promise.resolve(({ status: 200, body: 'success' }));
   });
   promise.catch(() => Error());
 }
