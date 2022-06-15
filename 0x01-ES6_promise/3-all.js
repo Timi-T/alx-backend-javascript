8ac79e49723a867e0653639bf7f338d6c1bfc3ae
Timi-T <timiopebode@gmail.com>
2022-06-15 12:19:15 +0000

    promise.all

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -3,19 +3,14 @@ import { uploadPhoto, createUser } from './utils';
 export default function handleProfileSignup() {
   const promise1 = uploadPhoto();
   const promise2 = createUser();
-  promise1.then(
-    (photoInfo) => {
-      promise2.then(
-        (userInfo) => {
-          console.log(`${photoInfo.body} ${userInfo.firstName} ${userInfo.lastName}`);
-        },
-        () => {
-          console.log(Error('Signup system offline'));
-        },
-      );
+
+  Promise.all([promise1, promise2]).then(
+    (data) => {
+      const output = `${data[0].body} ${data[1].firstName} ${data[1].lastName}`;
+      console.log(output);
     },
     () => {
-      console.log(Error('Signup system offline'));
+      console.log('Signup system offline');
     },
   );
 }
