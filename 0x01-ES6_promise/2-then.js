3f67966fee40a308896efe80e96de967bc9c72ef
Timi-T <timiopebode@gmail.com>
2022-06-15 11:03:31 +0000

    two functions in .then

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,7 +1,9 @@
 export default function handleResponseFromAPI(promise) {
-  promise.then(() => {
-    console.log('Got a response from the API');
-    Promise.resolve(({ status: 200, body: 'success' }));
-  });
-  promise.catch(() => Error());
+  promise.then(
+    () => {
+      console.log('Got a response from the API');
+      return ({ status: 200, body: 'Success' });
+    },
+    () => Error(),
+  );
 }
