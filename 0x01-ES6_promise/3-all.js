432bcb66f5d76f43ef01fba2b19d9386864a187a
Timi-T <timiopebode@gmail.com>
2022-06-15 20:29:45 +0000

    return a promise

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -4,10 +4,11 @@ export default function handleProfileSignup() {
   const promise1 = uploadPhoto();
   const promise2 = createUser();
 
-  Promise.all([promise1, promise2]).then(
+  return Promise.all([promise1, promise2]).then(
     (data) => {
       const output = `${data[0].body} ${data[1].firstName} ${data[1].lastName}`;
       console.log(output);
-    }
-  );
+    },
+  )
+    .catch(() => console.log('Signup system offline'));
 }
