4ee2c190274c8a473ce8675806df825a5b764d28
Timi-T <timiopebode@gmail.com>
2022-06-15 18:42:48 +0000

    changed reason to value

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -4,5 +4,13 @@ import uploadPhoto from './5-photo-reject';
 export default function handleProfileSignup(firstName, lastName, fileName) {
   const promise1 = signUpUser(firstName, lastName);
   const promise2 = uploadPhoto(fileName);
-  return Promise.allSettled([promise1, promise2]).then((data) => data);
+  return Promise.allSettled([promise1, promise2]).then((data) => {
+    for (const eachData of data) {
+      if ('reason' in eachData) {
+        eachData.value = eachData.reason;
+        delete eachData.reason;
+      }
+    }
+    return data;
+  });
 }
