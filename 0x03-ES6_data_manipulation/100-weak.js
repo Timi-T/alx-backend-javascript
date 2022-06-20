b78296d873403ab621d3346de4c92648c3209fb4
Timi-T <timiopebode@gmail.com>
2022-06-20 16:13:48 +0000

    little fix

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -3,10 +3,10 @@ export function queryAPI(endpoint) {
   if (!weakMap.get(endpoint)) {
     weakMap.set(endpoint, 1);
   } else {
+    const temp = weakMap.get(endpoint);
+    weakMap.set(endpoint, temp + 1);
     if (weakMap.get(endpoint) >= 5) {
       throw Error('Endpoint load is high');
     }
-    const temp = weakMap.get(endpoint);
-    weakMap.set(endpoint, temp + 1);
   }
 }
