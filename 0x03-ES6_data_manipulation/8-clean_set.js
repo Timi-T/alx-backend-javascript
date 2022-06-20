2db41cf7ce2f34a187ea8092a42e7a238e5e885a
Timi-T <timiopebode@gmail.com>
2022-06-20 17:33:15 +0000

    handle edge cases

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -5,7 +5,7 @@ export default function cleanSet(set, startString) {
   let compoundStr = '';
   set.forEach((str) => {
     const compare = str.split(startString);
-    if (compare.length > 1) {
+    if (compare.length > 1 && compare[0] === '') {
       compoundStr ? compoundStr += `-${compare[1]}` : compoundStr += `${compare[1]}`; // eslint-disable-line no-unused-expressions
     }
   });
