7b85812f7c3e267b35e1659ca1654c99f10b0271
Timi-T <timiopebode@gmail.com>
2022-06-20 13:35:54 +0000

    split a string

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,13 @@
+export default function cleanSet(set, startString) {
+  if (!startString) {
+    return '';
+  }
+  let compoundStr = '';
+  set.forEach((str) => {
+    const compare = str.split(startString);
+    if (compare.length > 1) {
+      compoundStr ? compoundStr += `-${compare[1]}` : compoundStr += `${compare[1]}`; // eslint-disable-line no-unused-expressions
+    }
+  });
+  return compoundStr;
+}
