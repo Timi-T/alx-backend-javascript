d458f2c5b6cfee55eabaedfd129fec125237467b
Timi-T <timiopebode@gmail.com>
2022-08-08 22:48:59 +0100

    syntax correction

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,7 +1,7 @@
 // Simple program to write to stdout
 
 console.log('Welcome to Holberton School, what is your name?');
-process.stdin.on('data', data => {
+process.stdin.on('data', (data) => {
   process.stdout.write(`Your name is: ${data.toString()}`);
   console.log('This important software is now closing');
   process.exit();
