1d366c9e98fdcb7c7678b4d2d1ec9d4da9b86422
Timi-T <timiopebode@gmail.com>
2022-08-08 15:56:07 +0100

    advanced express server

---
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -0,0 +1,71 @@
+const express = require('express');
+const app = express();
+
+function countStudents (path) {
+  const fs = require('fs');
+
+  const makePromise = () => {
+    return new Promise((resolve, reject) => {
+      fs.readFile(path, 'utf-8', (error, data) => {
+        if (error) {
+          throw new Error('Cannot load the database');
+        } else {
+          let dataArray = data.split('\n');
+          dataArray = dataArray.filter((str) => {
+            return str !== '';
+          });
+
+          const newArray = [];
+          const fieldCount = {};
+          const fieldNames = {};
+
+          for (let i = 0; i < dataArray.length; i++) {
+            dataArray[i] = dataArray[i].split(',');
+            if (i !== 0) {
+              const obj = {};
+              for (let j = 0; j < dataArray[0].length; j++) {
+                obj[dataArray[0][j]] = dataArray[i][j];
+              }
+              const field = obj.field;
+              let count = 0;
+              if (fieldCount[field]) {
+                count = fieldCount[field];
+              }
+              fieldCount[field] = count + 1;
+              if (!fieldNames[field]) {
+                fieldNames[field] = [];
+              }
+              const names = fieldNames[field];
+              names.push(obj.firstname);
+              fieldNames[field] = names;
+              newArray.push(obj);
+            }
+          }
+          let allLines = `Number of students: ${newArray.length}`;
+          Object.keys(fieldCount).forEach(field => {
+            allLines += `\nNumber of students in ${field}: ${fieldCount[field]}. List:${fieldNames[field].map(firstname => {
+              return ' ' + firstname;
+            })}`;
+          });
+          return resolve(allLines);
+        }
+      });
+    });
+  };
+  return makePromise();
+}
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.get('/students', (req, res) => {
+  countStudents('database.csv')
+    .then((response) => {
+      res.send('This is the list of our students\n' + response);
+    });
+});
+
+app.listen(1245);
+
+module.exports = app;
