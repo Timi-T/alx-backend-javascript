e57e1fe4b60a997dc69197a49332c9d7287fc607
Timi-T <timiopebode@gmail.com>
2022-08-09 08:36:53 +0100

    documentation

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -1,6 +1,8 @@
-const express = require('express');
-const app = express();
+// Create a simple express server
 
+const express = require('express');
+
+const app = express();
 app.get('/', (req, res) => {
   res.send('Hello Holberton School!');
 });
