1eab2f2cf710b24b5e0aa5221156f2956737a6ad
Timi-T <timiopebode@gmail.com>
2022-08-09 16:27:40 +0100

    no routes

--- 0x05-Node_JS_basic/full_server/server.js
+++ 0x05-Node_JS_basic/full_server/server.js
@@ -4,9 +4,9 @@ const express = require("express");
 const app = express();
 const router = require("./routes/index");
 
-app.use('/', router);
-app.use('/students', router);
-app.use('/students/:major', router);
+//app.use('/', router);
+//app.use('/students', router);
+//app.use('/students/:major', router);
 app.listen(1245);
 
 export default app;
