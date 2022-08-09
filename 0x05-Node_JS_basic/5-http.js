d791a5d4f18131829739a64b6900dad34a49f691
Timi-T <timiopebode@gmail.com>
2022-08-09 09:59:22 +0100

    dedine function in app

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -2,7 +2,7 @@
 
 const http = require('http');
 
-function countStudents (path) {
+/*function countStudents (path) {
   const fs = require('fs');
 
   const makePromise = () => {
@@ -54,11 +54,67 @@ function countStudents (path) {
     });
   };
   return makePromise();
-}
+}*/
 
 const path = process.argv[2];
 
 const app = http.createServer((request, response) => {
+
+  function countStudents (path) {
+    const fs = require('fs');
+  
+    const makePromise = () => {
+      return new Promise((resolve, reject) => {
+        fs.readFile(path, 'utf-8', (error, data) => {
+          if (error) {
+            reject(new Error('Cannot load the database'));
+          } else {
+            let dataArray = data.split('\n');
+            dataArray = dataArray.filter((str) => {
+              return str !== '';
+            });
+  
+            const newArray = [];
+            const fieldCount = {};
+            const fieldNames = {};
+  
+            for (let i = 0; i < dataArray.length; i++) {
+              dataArray[i] = dataArray[i].split(',');
+              if (i !== 0) {
+                const obj = {};
+                for (let j = 0; j < dataArray[0].length; j++) {
+                  obj[dataArray[0][j]] = dataArray[i][j];
+                }
+                const field = obj.field;
+                let count = 0;
+                if (fieldCount[field]) {
+                  count = fieldCount[field];
+                }
+                fieldCount[field] = count + 1;
+                if (!fieldNames[field]) {
+                  fieldNames[field] = [];
+                }
+                const names = fieldNames[field];
+                names.push(obj.firstname);
+                fieldNames[field] = names;
+                newArray.push(obj);
+              }
+            }
+            let allLines = `Number of students: ${newArray.length}`;
+            Object.keys(fieldCount).forEach(field => {
+              allLines += `\nNumber of students in ${field}: ${fieldCount[field]}. List:${fieldNames[field].map(firstname => {
+                return ' ' + firstname;
+              })}`;
+            });
+            return resolve(allLines);
+          }
+        });
+      });
+    };
+    return makePromise();
+  }
+  
+
   if (request.url === '/') {
     response.write('Hello Holberton School!');
     response.end();
