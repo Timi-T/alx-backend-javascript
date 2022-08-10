97d7dc1095dea16deeabf0ac1b7e0faaecdd51cd
Timi-T <timiopebode@gmail.com>
2022-08-10 16:25:59 +0100

    test in a context

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -5,10 +5,12 @@ const { describe } = require('mocha');
 const calculateNumber = require('./0-calcul');
 
 describe('calculateNumber', function () {
-  it('adds the round of two numbers', function () {
-    assert.equal(calculateNumber(1, 3), 4);
-    assert.equal(calculateNumber(1, 3.7), 5);
-    assert.equal(calculateNumber(1.2, 3.7), 5);
-    assert.equal(calculateNumber(1.5, 3.7), 6);
-  });
+  context('Test calculateNumber function', function test1() {
+    it('adds the round of two numbers', function testSum() {
+      assert.equal(calculateNumber(1, 3), 4);
+      assert.equal(calculateNumber(1, 3.7), 5);
+      assert.equal(calculateNumber(1.2, 3.7), 5);
+      assert.equal(calculateNumber(1.5, 3.7), 6);
+    });
+  })
 });
