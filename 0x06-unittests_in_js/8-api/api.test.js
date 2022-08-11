ee83e26ee8c86bdfaa9dc1494bc9bbbde5df8532
Timi-T <timiopebode@gmail.com>
2022-08-11 19:30:46 +0100

    more tests

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -12,11 +12,17 @@ describe('Test api', function testApi() {
     res = request.get('http://localhost:7865/', (err, res, data) => {
       expect(res.statusCode).to.be.equal(200);
     });
-  })
+  });
 
   it('Test api for correct data', function testcode() {
     res = request.get('http://localhost:7865/', (err, res, data) => {
       expect(data).to.be.equal('Welcome to the payment system');
     });
-  })
+  });
+
+  it('Test api for correct data', function testcode() {
+    res = request.get('http://localhost:7865/', (err, res, data) => {
+      expect(data.length).to.be.equal(29);
+    });
+  });
 })
