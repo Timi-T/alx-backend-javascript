24aabb37192670142c5f10b7784e558c1b44f549
Timi-T <timiopebode@gmail.com>
2022-08-11 17:17:16 +0100

    separate tests

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -5,12 +5,16 @@ const { describe } = require('mocha');
 const calculateNumber = require('./0-calcul');
 
 describe('calculateNumber', function () {
-  //context('Test calculateNumber function', function test1() {
   it('adds the round of two numbers', function testSum() {
     assert.equal(calculateNumber(1, 3), 4);
+  });
+  it('adds the round of two numbers', function testSum2() {
     assert.equal(calculateNumber(1, 3.7), 5);
+  });
+  it('adds the round of two numbers', function testSum3() {
     assert.equal(calculateNumber(1.2, 3.7), 5);
+  });
+  it('adds the round of two numbers', function testSum4() {
     assert.equal(calculateNumber(1.5, 3.7), 6);
   });
-  //})
 });
