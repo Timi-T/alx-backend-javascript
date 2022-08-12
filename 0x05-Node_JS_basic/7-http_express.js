b51eef68da79c45dc3a647a48a50e35663ebbbe5
Timi-T <timiopebode@gmail.com>
2022-08-12 08:42:57 +0100

    eslint

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -1,59 +1,55 @@
 // Express application with complex output
 
 const express = require('express');
+
 const app = express();
 
-function countStudents (path) {
-  const fs = require('fs');
+const fs = require('fs');
 
-  const makePromise = () => {
+function countStudents(path) {
+  function makePromise() {
     return new Promise((resolve, reject) => {
       fs.readFile(path, 'utf-8', (error, data) => {
         if (error) {
-          reject(new Error('Cannot load the database'));
-        } else {
-          let dataArray = data.split('\n');
-          dataArray = dataArray.filter((str) => {
-            return str !== '';
-          });
-
-          const newArray = [];
-          const fieldCount = {};
-          const fieldNames = {};
-
-          for (let i = 0; i < dataArray.length; i++) {
-            dataArray[i] = dataArray[i].split(',');
-            if (i !== 0) {
-              const obj = {};
-              for (let j = 0; j < dataArray[0].length; j++) {
-                obj[dataArray[0][j]] = dataArray[i][j];
-              }
-              const field = obj.field;
-              let count = 0;
-              if (fieldCount[field]) {
-                count = fieldCount[field];
-              }
-              fieldCount[field] = count + 1;
-              if (!fieldNames[field]) {
-                fieldNames[field] = [];
-              }
-              const names = fieldNames[field];
-              names.push(obj.firstname);
-              fieldNames[field] = names;
-              newArray.push(obj);
-            }
-          }
-          let allLines = `Number of students: ${newArray.length}`;
-          Object.keys(fieldCount).forEach((field) => {
-            allLines += `\nNumber of students in ${field}: ${fieldCount[field]}. List:${fieldNames[field].map((firstname) => {
-              return ' ' + firstname;
-            })}`;
-          });
-          return resolve(allLines);
+          return reject(new Error('Cannot load the database'));
         }
+        let dataArray = data.split('\n');
+        dataArray = dataArray.filter((str) => str !== '');
+
+        const newArray = [];
+        const fieldCount = {};
+        const fieldNames = {};
+
+        for (let i = 0; i < dataArray.length; i += 1) {
+          dataArray[i] = dataArray[i].split(',');
+          if (i !== 0) {
+            const obj = {};
+            for (let j = 0; j < dataArray[0].length; j += 1) {
+              obj[dataArray[0][j]] = dataArray[i][j];
+            }
+            const { field } = obj;
+            let count = 0;
+            if (fieldCount[field]) {
+              count = fieldCount[field];
+            }
+            fieldCount[field] = count + 1;
+            if (!fieldNames[field]) {
+              fieldNames[field] = [];
+            }
+            const names = fieldNames[field];
+            names.push(obj.firstname);
+            fieldNames[field] = names;
+            newArray.push(obj);
+          }
+        }
+        let allLines = `Number of students: ${newArray.length}`;
+        Object.keys(fieldCount).forEach((field) => {
+          allLines += `\nNumber of students in ${field}: ${fieldCount[field]}. List:${fieldNames[field].map((firstname) => ` ${firstname}`)}`;
+        });
+        return resolve(allLines);
       });
     });
-  };
+  }
   return makePromise();
 }
 
@@ -69,11 +65,11 @@ app.get('/', (req, res) => {
 app.get('/students', (req, res) => {
   countStudents(path)
     .then((data) => {
-      res.send('This is the list of our students\n' + data);
+      res.send(`This is the list of our students\n ${data}`);
     })
     .catch((err) => {
-      res.send('This is the list of our students\n' + err.message);
-    })
+      res.send(`This is the list of our students\n ${err.message}`);
+    });
 });
 
 app.listen(1245);
